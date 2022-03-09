3a4bd06a41e7d16c2a0b5852e61ad16c6194e300
vildan-b <vildanb@gmx.de>
2022-03-09 14:20:11 +0100

    test1

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="container">
+        <form>
+        <div v-if="!isSubmitted">    
+           <div class="row">
+               <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+               <div class="col-md-12">
+                             <appNamesurname v-model="fullname"></appNamesurname>
+
+                  <div class="form-group">
+                    <label for="email">E-Mail</label>
+                    <input type="text" 
+                    id="email" 
+                    class="form-control" 
+                    v-model="email"
+                    >
+                  </div>
+                  <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" 
+                    id="password" 
+                    class="form-control" 
+                    v-model="password"
+                    >
+                  </div>
+                </div>
+                <div>
+                <label> <input type="radio" value="Female" v-model="gender"> Female </label>
+                <label> <input type="radio" value="Male" v-model="gender"> Male </label>
+
+                </div>
+                <button @click.prevent="submit()"> Sign up</button>
+                </div>
+
+                  
+                   
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div v-if="isSubmitted" class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <h4> Information You Entered</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Fullname: {{ fullname}} </p>
+                        <p>E-mail: {{ email}}</p>
+                        <p>Password: {{password}}</p>
+                        <p>Gender: {{gender}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import NameSurname from './NameSurname'
+    export default {
+    
+        data(){
+            return{
+                isSubmitted:false,
+                    fullname:"",
+                    email:"",
+                    password:"",
+                    gender:""
+                
+            }
+        },
+        methods:{
+            submit(){
+                this.isSubmitted = true;
+
+            }
+        },  
+          components: {
+            appNamesurname : NameSurname
+        },   
+      
+    }
+</script>
+
+<style>
+</style>
---
+++ src/NameSurname.vue
@@ -0,0 +1,48 @@
+<template>
+<div >
+<div class="form-group">
+                    <label> Firstname</label>
+                    <input type="text" 
+                    class="form-control" 
+                    :value="firstName" @input="nameChanged(true,$event)"
+                    >
+                  </div>
+                  <div class="form-group">
+                    <label>Lastname</label>
+                    <input type="text" 
+                    class="form-control" 
+                    :value="lastName"
+                    @input="nameChanged(false,$event)"
+                    >
+                  </div>
+
+</div>
+</template>
+<script>
+export default{
+   props:["value"],
+
+methods:{
+    nameChanged(isFirst, event){
+        let fullname="";
+        if(isFirst){
+            fullname = event.target.value + " " + this.lastName;
+        } else {
+        fullname = this.firstName + " "+event.target.value;
+
+        }
+        this.value = fullname;
+        this.$emit("input", this.value);
+    }
+    
+},
+computed:{
+    firstName(){
+        return this.value.split(" ")[0];
+    },
+        lastName(){
+        return this.value.split(" ")[1];
+    }
+}
+}
+</script>
